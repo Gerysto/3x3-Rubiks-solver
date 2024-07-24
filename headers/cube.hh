2dd54398494d2022562c7b4ded67256e8d2a4433
Gerard Félez Guerrero <gerardfelez2@gmail.com>
2024-07-24 21:57:57 +0200

    Added some documentation to the Cube class.

--- headers/cube.hh
+++ headers/cube.hh
@@ -6,7 +6,27 @@ using namespace std;
 /*
  * The 'cube' class represents a 3x3 rubik's cube, which is defined entirely off of 
  * it's 'state' which is the position of it's pieces and it's orientation, which is 
- * used to tell what side should move when doing a move.  
+ * used to tell what side should move when doing a move. 
+ * 
+ * Allowed moves: 
+ *
+ *      (Clockwhise)
+ * 
+ *      R, L, U, D, F, B
+ *      r, l, u, d, f, b
+ *      M, E, S, X, Y, Z 
+ *      
+ *      (Counter-clockwhise)
+ * 
+ *      R', L', U', D', F', B'
+ *      r', l', u', d', f', b'
+ *      M', E', S', X', Y', Z'
+ * 
+ *      (Double move)
+ * 
+ *      R2, L2, U2, D2, F2, B2
+ *      r2, l2, u2, d2, f2, b2
+ *      M2, E2, S2, X2, Y2, Z2
 */
 
 class Cube {
@@ -23,7 +43,7 @@ class Cube {
      * each center color is assigned a value from 0 to 5
      * each side of the cube corresponds with an index of the array.
      * 
-     * Color values:
+     * Color IDs:
      * 
      *      red = 0
      *      blue = 1
@@ -32,7 +52,7 @@ class Cube {
      *      orange = 4
      *      yellow = 5
      * 
-     * Indexes of the sides:
+     * Side IDs:
      *      
      *      top = 0
      *      back = 1
@@ -47,18 +67,43 @@ class Cube {
     u_int8_t orientation[6];
 
     /*
-     * Default constructor 
+     * Default constructor.
+     * Creates a solved cube with the white top, green front orientation.
     */
-   
     Cube();
 
+    /*
+     * Creates a cube given it's state and initial orientation.
+    */
     Cube(State state, u_int8_t orientation);
 
-    void turn_face(u_int8_t face);
+    /*
+     * Turns a face, given the id of it's center color.
+     * (e.g. allows you to turn the white face)
+     * 
+     * Check the 'Color IDs' table above.
+    */
+    void turn_face(u_int8_t face_color, bool clockwhise);
 
-    void turn_side(u_int8_t side);
-    
+    /*
+     * Turns a side given the id of it's relative position.
+      (e.g. allows you to turn the front side)
+
+      Check the 'Side IDs' table above.
+    */
+    void turn_side(u_int8_t side, bool clockwhise);
+
+    /*
+     * Given a move in the standard rubik's cube notation, 
+     * performs set move in the cube. 
+     * 
+     * (check the table of allowed moves from above)
+    */
     void move(string move);
 
+    /*
+     * Given a string containing a sequence of moves separated with spaces,
+     * it performs the sequence set sequence on the cube.
+     */
     void move_sequence(string sequence);
 };
