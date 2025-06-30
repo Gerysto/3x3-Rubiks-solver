48b237b32f5c957b42cfc944b8567f46fd16201e
Gerard Félez Guerrero <gerardfelez2@gmail.com>
2025-06-30 12:13:40 +0200

    The tringle gets drawn :D

--- index.js
+++ index.js
@@ -5,19 +5,46 @@ async function start() {
         throw new Error("Failed to start WebGL!");
     }
     
-    let vertShaderSource = getShaderSource("shaders/shader.vert");
-    let fragShaderSource = getShaderSource("shaders/shader.frag");
+    // Setup the program (vertex + fragment shaders)
+    let vertShaderSource = await getShaderSource("shaders/shader.vert");
+    let fragShaderSource = await getShaderSource("shaders/shader.frag");
 
     let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertShaderSource);
     let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragShaderSource);
 
     let program = createProgram(gl,vertexShader,fragmentShader);
+    gl.useProgram(program);
+    // Get attribute locations:
+    let vertexLoc = gl.getAttribLocation(program, "vertex");
+    console.log("Vertex loc = " + vertexLoc);
+    // Set viewport size:
+    gl.viewport(0,0,gl.canvas.width, gl.canvas.height);
+
+    // Create and fill-in the buffers
+    let vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+    let vertices = [
+        0,0,0,
+        1,0,0,
+        1,1,0
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    
+    // set up the VAO 
+    let vao = gl.createVertexArray();
+    gl.bindVertexArray(vao);
+    gl.enableVertexAttribArray(vertexLoc);
+
+    // Specify how to read the data:
+    gl.vertexAttribPointer(vertexLoc, 3, gl.FLOAT, false, 0, 0);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
 
 // Returns the contents of the file in the url provided.
 async function getShaderSource(url) {
     let contents = await fetch(url);
-    if (!contents.ok()) {
+    if (!contents.ok) {
         throw new Error("Failed to read contents of: " + url);
     }
     let result = await contents.text();
@@ -32,21 +59,23 @@ function createShader(gl, type, source) {
     if (success) {
         return shader;
     }
-
+    console.log(source);
     console.log(gl.getShaderInfoLog(shader));
     gl.deleteShader(shader);
+    throw new Error("Failed to create Shader: " + source);
 }
 
 
 function createProgram(gl, vertexShader, fragmentShader) {
     var program = gl.createProgram();
-    gl.attatchShader(program, vertexShader);
-    gl.attatchShader(program, fragmentShader);
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
     gl.linkProgram(program);
     var success = gl.getProgramParameter(program, gl.LINK_STATUS);
     if (success) {
         return program;
     }
-    console.log(gl.getProgramInforLog(program));
+    console.log(gl.getProgramInfoLog(program));
     gl.deleteProgram(program);
+    throw new Error("Failed to create Program!");
 }
