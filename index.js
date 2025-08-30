f6798bfe8f718077d5fc56bf8917a67a435e6818
Gerard Félez Guerrero <gerardfelez2@gmail.com>
2025-08-30 14:45:47 +0200

    Added sample .obj file and implemented a function to create the buffers for a model

--- index.js
+++ index.js
@@ -44,31 +44,37 @@ function initialize(gl) {
     resizeCanvasToDisplaySize(gl.canvas);
     gl.viewport(0,0,gl.canvas.width,gl.canvas.height);
     console.log(gl.canvas.width, gl.canvas.height);
+
     // Get attribute locations:
-    let vertexLoc = gl.getAttribLocation(g.program, "vertex");
-    let colorLoc = gl.getAttribLocation(g.program, "color");
-
-    console.log("Vertex loc = " + vertexLoc);
-    console.log("Color loc = " + colorLoc);
-
+    g.vertexLoc = gl.getAttribLocation(g.program, "vertex");
+    g.normalLoc = gl.getAttribLocation(g.program, "normal");
+    g.matambLoc = gl.getAttribLocation(g.program, "matamb");
+    g.matdiffLoc = gl.getAttribLocation(g.program, "matdiff");
+    g.matspecLoc = gl.getAttribLocation(g.program, "matspec");
+    g.matshinLoc = gl.getAttribLocation(g.program, "matshin");
     
     // set up the VAO 
-    let vao = gl.createVertexArray();
+    const url = "Patricio.obj";
+    let vao_model = createBuffersModel(url);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+}
+
+// Creates and fills the requiered Web-GL buffers to render the model.
+// Returns the model's VAO (vertex array object)
+function createbuffersModel(url) {
+    vao = gl.createVertexArray();
     gl.bindVertexArray(vao);
 
+    model = new Model();
 
+    model.load(url);
     // Create and fill-in the buffers
-    let vertices = [
-        0,0,0,
-        1,0,0,
-        1,1,0
-    ];
-
-    let colors = [
-        1,0,0,
-        0,1,0,
-        0,0,1
-    ];
+    let vertices = model.get_VBO_vertices();
+    let normals = model.get_VBO_normals();
+    let matamb = model.get_VBO_matamb();
+    let matdiff = model.get_VBO_matdiff();
+    let matspec = model.get_VBO_matspec();
+    let matshin = model.get_VBO_matshin();
 
     // Vertex buffer: 
     let vertexBuffer = gl.createBuffer();
@@ -77,12 +83,42 @@ function initialize(gl) {
     gl.enableVertexAttribArray(vertexLoc);
     gl.vertexAttribPointer(vertexLoc, 3, gl.FLOAT, false, 0, 0);
     
-    // Color buffer:
-    let colorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-    gl.enableVertexAttribArray(colorLoc);
-    gl.vertexAttribPointer(colorLoc, 3, gl.FLOAT, false, 0, 0);
+    // Normals buffer: 
+    let normalBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(g.normalLoc);
+    gl.vertexAttribPointer(g.normalLoc, 3, gl.FLOAT, false, 0, 0);
+
+    // Ambient material parameter buffer: 
+    let matambBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, matambBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(matamb), gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(g.matambLoc);
+    gl.vertexAttribPointer(g.matambLoc, 3, gl.FLOAT, false, 0, 0);
+        
+    // Diffuse material parameter buffer: 
+    let matdiffBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, matdiffbuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(matdiff), gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(g.matdiffLoc);
+    gl.vertexAttribPointer(g.matdiffLoc, 3, gl.FLOAT, false, 0, 0);
     
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    // Specular material parameter buffer: 
+    let matspecBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, matspecBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(matspec), gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(g.matspecLoc);
+    gl.vertexAttribPointer(g.matspecLoc, 3, gl.FLOAT, false, 0, 0);
+    
+    
+    // shinyness material parameter buffer: 
+    let matshinBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, matshinBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(matshin), gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(g.matshinLoc);
+    gl.vertexAttribPointer(g.matshinLoc, 1, gl.FLOAT, false, 0, 0);
+    
+    gl.bindVertexArray(null);
+    return vao;
 }
