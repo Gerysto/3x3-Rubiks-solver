69ef322a6bba1a69b519eace3d8d36866aa34fe5
Gerard Félez Guerrero <gerard.felez@estudiantat.upc.edu>
2026-03-10 00:25:52 +0100

    Moves 'resizeCanvasToDisplaySize' function to Renderer class.

--- Viewer/src/engine/Renderer.ts
+++ Viewer/src/engine/Renderer.ts
@@ -22,7 +22,7 @@ export class Renderer {
             throw new Error("Failed to start WebGL!");
         }
 
-        this.gl.viewport(0,0, width, height);
+        this.gl.viewport(0,0, canvas.width, canvas.height);
         this.gl.enable(this.gl.DEPTH_TEST);
         this.gl.clearColor(0.8,0.8,0.8,1);
     }
@@ -33,10 +33,15 @@ export class Renderer {
                 this.gl.COLOR_BUFFER_BIT |
                 this.gl.DEPTH_BUFFER_BIT
             );
-
+            if (Renderer.resizeCanvasToDisplaySize(this.gl)) {
+                // Re-calculate projection view / projection matrices!
+            };
+            
             for (const obj of scene.objects) {
+                //console.log("Inside start function", obj);
                 this.drawObject(obj, camera);
             }
+            //requestAnimationFrame(loop);
         }
 
         loop();
@@ -51,10 +56,19 @@ export class Renderer {
         const NM = this.normalMatrix(VM, TG);       // Normal matrix
         const PM = camera.getProjectionMatrix(ra);  // Projection matrix
         
-        VM.setUniform(gl, this.program.VMLoc, false);
-        TG.setUniform(gl, this.program.TGLoc, false);
-        NM.setUniform(gl, this.program.NMLoc, false);
-        PM.setUniform(gl, this.program.PMLoc, false);
+        const F: GLboolean = false;
+
+
+        /*console.log("Des de renderer: ", this.program.VMLoc);
+        console.log("Des de renderer: ", this.program.TGLoc);
+        console.log("Des de renderer: ", this.program.NMLoc);
+        console.log("Des de renderer: ", this.program.PMLoc);*/
+
+        VM.setUniform(gl, this.program.VMLoc, F);
+        TG.setUniform(gl, this.program.TGLoc, F);
+        NM.setUniform(gl, this.program.NMLoc, F);
+        PM.setUniform(gl, this.program.PMLoc, F);
+
 
         gl.bindVertexArray(obj.vao);
         gl.drawArrays(gl.TRIANGLES, 0, obj.vertices.length / 3);
@@ -67,5 +81,21 @@ export class Renderer {
         NM.invert();
         NM.transpose();
         return NM;
+    }
+
+
+    private static resizeCanvasToDisplaySize(gl:WebGL2RenderingContext) : boolean {
+        const canvas : HTMLCanvasElement = gl.canvas as HTMLCanvasElement;
+        const dpr = window.devicePixelRatio || 1;
+        const displayWidth = Math.floor(canvas.clientWidth * dpr);
+        const displayHeight = Math.floor(canvas.clientHeight * dpr);
+
+        if (canvas.width !== displayWidth || canvas.height !== displayHeight) {
+            canvas.width = displayWidth;
+            canvas.height = displayHeight;
+            gl.viewport(0,0, canvas.width, canvas.height);
+            return true;
+        }
+        return false;
     }
 }
