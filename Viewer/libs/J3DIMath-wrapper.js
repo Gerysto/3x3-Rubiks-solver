9bb8de5962138d0b46bf29f0ed7d3ee195dcbe5c
Gerard Félez Guerrero <gerard.felez@estudiantat.upc.edu>
2026-03-09 15:57:45 +0100

    Modifies J3DIMaths wrapper to export J3DIVector3 in addition to J3DIMatrix4

--- Viewer/libs/J3DIMath-wrapper.js
+++ Viewer/libs/J3DIMath-wrapper.js
@@ -1,2 +1,7 @@
 import './J3DIMath.js';
-export default window.J3DIMatrix4;
+
+const J3DIMatrix4 = window.J3DIMatrix4;
+const J3DIVector3 = window.J3DIVector3;
+
+export { J3DIMatrix4, J3DIVector3 };
+export default J3DIMatrix4;
