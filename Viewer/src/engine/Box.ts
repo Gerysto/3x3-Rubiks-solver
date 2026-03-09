10cba4cd05381d7bc6a2267dcb2a536df411c828
Gerard Félez Guerrero <gerard.felez@estudiantat.upc.edu>
2026-03-09 13:31:56 +0100

    Implements height and baseCenter methods of class Box

--- Viewer/src/engine/Box.ts
+++ Viewer/src/engine/Box.ts
@@ -9,9 +9,18 @@ export class Box {
         this.max = v_max;
     }
 
-    center() {
+    center() : Vec3{
         return Vec3.add(this.min, this.max).scale(0.5);
     }
 
-    
+    baseCenter() : Vec3 {
+        return new Vec3(
+            (this.min.x+this.max.x)/2.0,
+            (this.min.y),
+            (this.min.z+this.max.z)/2.0);
+    }
+
+    height() : number {
+        return this.max.y - this.min.y;
+    }
 }
