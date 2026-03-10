b7efcdc403780df9881ac23c04386b0c0fce6c96
Gerard Félez Guerrero <gerard.felez@estudiantat.upc.edu>
2026-03-10 22:08:30 +0100

    Adds a prototype 'perform_move' method to 'rubiks_cube.ts'

--- Viewer/src/rubikscube/rubiks_cube.ts
+++ Viewer/src/rubikscube/rubiks_cube.ts
@@ -2,6 +2,7 @@ import {Object} from '../engine/Object.js';
 import { Scene } from '../engine/Scene.js';
 import { ShaderProgram } from '../shaders/ShaderProgram.js';
 import { corner_TG, edge_TG, center_TG } from './piece_transformations.js';
+import { PIECES_INVOLVED } from './rubiks_constants.js';
 
 const CORNER_MODELS_URL = 'assets/models/corners/c#.obj';
 const EDGE_MODELS_URL   = 'assets/models/edges/e#.obj';
@@ -53,5 +54,39 @@ export class RubiksCube {
         for (let obj of this.corners) scene.addObject(obj);
         for (let obj of this.edges)   scene.addObject(obj);
         for (let obj of this.centers) scene.addObject(obj);
+    }
+
+    /**
+     * Partilly performs one of the moves: {U, R, F, D, L, B}, according to the interpolation value @param t provided 
+     * @param move one of the outer moves in cubing notation {U, R, F, D, L, B}
+     * @param clockwhise whether the turn should be clockwhise {U}, or anticlockwhise {U'}
+     * @param t interpolation parameter (ranging from 0 to 1)
+     */
+    perform_move(move: string, clockwhise: boolean, t: number) {
+        const pieces_involved = PIECES_INVOLVED.get(move) as number[][];
+
+        // Corners
+        for (let i of pieces_involved[0]) {
+            // Divide index by 3 to conver from 'stiker index' to 'piece index'
+
+            this.corners[i/3].ModelTransform = new J3DIMatrix4();
+            this.corners[i/3].ModelTransform.rotate(90*t, 1, 0, 0);
+            this.corners[i/3].ModelTransform.multiply(corner_TG[i]);
+        }
+
+        // Edges: 
+        for (let i of pieces_involved[1])   {
+            // Divide index by 2 to conver from 'stiker index' to 'piece index'
+            this.edges[i/2].ModelTransform = new J3DIMatrix4();
+            this.edges[i/2].ModelTransform.rotate(90*t, 1, 0, 0);
+            this.edges[i/2].ModelTransform.multiply(edge_TG[i]);
+        }
+
+        // Centers: 
+        for (let i of pieces_involved[2]) {
+            this.centers[i].ModelTransform = new J3DIMatrix4();
+            this.centers[i].ModelTransform.rotate(90*t, 1, 0, 0);
+            this.centers[i].ModelTransform.multiply(center_TG[i]);
+        }
     }
 }
