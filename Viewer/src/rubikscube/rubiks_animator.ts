50c154a4f66248e34892247ddb10747eb797f225
Gerard Félez Guerrero <gerard.felez@estudiantat.upc.edu>
2026-03-11 17:52:51 +0100

    Creates an animator class (which allows you to enqueue moves and sequences of moves and update the cube accordingly

---
+++ Viewer/src/rubikscube/rubiks_animator.ts
@@ -0,0 +1,82 @@
+import { CubeController } from '../../libs/cube_lib.js';
+import { State, RubiksCube } from '../rubikscube/rubiks_cube.js';
+
+const TPS: number = 5; // Turns per second 
+
+export class RubiksAnimator {
+
+    cube: RubiksCube;
+    progress: number;
+    queue: [State, string, boolean][];
+
+    cube_ctrl: any;
+
+    // (final_state, move, clockwhise?)
+    current_move: [State, string, boolean] | null;
+
+    constructor(cube: RubiksCube, ctrl: any) {
+        this.cube = cube;
+        this.progress = 0;
+        this.queue = [];
+        this.current_move = null;
+        this.cube_ctrl = ctrl;
+    }
+
+    enqueue(final_state: State, move: string, clockwhise: boolean) {
+        this.queue.push([final_state, move, clockwhise]);
+    }
+
+    update(dt: number) {
+        if (!this.current_move) {
+            if (this.queue.length == 0) return;
+            else {
+                this.current_move = this.queue.shift() as [State, string, boolean];
+            }
+        }
+
+        const [final_state, move, clockwhise] = this.current_move;
+
+        this.progress += TPS*dt;
+        this.cube.perform_move(move, clockwhise, this.progress);
+
+        if (this.progress > 1.0) {
+            this.progress = 0;
+            this.current_move = null;
+            this.cube.update_state(final_state.corners, final_state.edges);
+        }
+    }
+
+    vectorToArray(v: any): number[] {
+        let res = [];
+        for (let i = 0; i < v.size(); ++i) {
+            res.push(v.get(i));
+        }
+        return res;
+    }
+
+    enqueue_algorithm(algorithm: string) {
+        const seq = algorithm.split(" ");
+        
+        for (let move of seq) {
+            let clockwhise = true;
+            if (move.length == 2 && move[1] == "'") clockwhise = false;
+            if (move.length == 0) continue;
+            
+            this.cube_ctrl.execute_sequence_in_notation(move);
+            let c = this.vectorToArray(this.cube_ctrl.get_state_corners());
+            let e = this.vectorToArray(this.cube_ctrl.get_state_edges());
+            this.enqueue(new State(c,e), move[0], clockwhise);
+        }
+    }
+
+    /**
+     * 
+        let module = await createModule();
+        let ctrl = new module.CubeController();
+        ctrl.execute_sequence_in_notation("R");
+
+        const corner_state = this.vectorToArray(ctrl.get_state_corners());
+        const edge_state   = this.vectorToArray(ctrl.get_state_edges());
+        rubiks_cube.update_state(corner_state, edge_state);
+     */
+}
