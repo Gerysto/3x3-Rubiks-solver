6a0c7dbdee256d232ba9a633d6c2676e65ced539
Gerard Félez Guerrero <gerard.felez@estudiantat.upc.edu>
2026-03-11 17:55:03 +0100

    Fixes a bug with piece orientation when doing multiple consecutive moves

--- Viewer/src/rubikscube/rubiks_cube.ts
+++ Viewer/src/rubikscube/rubiks_cube.ts
@@ -2,22 +2,34 @@ import {MeshObject} from '../engine/MeshObject.js';
 import { Scene } from '../engine/Scene.js';
 import { ShaderProgram } from '../shaders/ShaderProgram.js';
 import { corner_TG, edge_TG, center_TG } from './piece_transformations.js';
-import { PIECES_INVOLVED, AXIS_OF_ROTATION } from './rubiks_constants.js';
+import { PIECES_INVOLVED, AXIS_OF_ROTATION, SOLVED } from './rubiks_constants.js';
 
 const CORNER_MODELS_URL = 'assets/models/corners/c#.obj';
 const EDGE_MODELS_URL   = 'assets/models/edges/e#.obj';
 const CENTER_MODELS_URL = 'assets/models/centers/f#.obj';
+
+export class State {
+    corners: number[];
+    edges: number[];
+
+    constructor(corners: number[], edges: number[]) {
+        this.corners = corners;
+        this.edges = edges;
+    }
+}
 
 export class RubiksCube {
 
     corners: MeshObject[];
     edges: MeshObject[];
     centers: MeshObject[];
+    state: State;
 
     constructor() {
         this.corners = [];
         this.edges = [];
         this.centers = [];
+        this.state = new State(SOLVED.CORNERS, SOLVED.EDGES);
     }
 
     private async load() {
@@ -80,19 +92,44 @@ export class RubiksCube {
         // Corners
         for (let i of pieces_involved[0]) {
             // Divide index by 3 to conver from 'stiker index' to 'piece index'
-            const piece = Math.floor(i/3);
+            const pos = Math.floor(i/3);
+            const piece = Math.floor(this.state.corners[pos]/3);
+            
+            /**
+             * i = 6 -> pos = 2
+             * piece = 6         stiker = 20
+             */
+
             this.corners[piece].ModelTransform = new J3DIMatrix4();
             this.corners[piece].ModelTransform.rotate(angle, x, y, z);
-            this.corners[piece].ModelTransform.multiply(corner_TG[piece]);
+            this.corners[piece].ModelTransform.multiply(corner_TG[pos]);
+
+            const orientation = this.state.corners[pos]%3;
+            if (orientation != 0) {
+                // Orient the corner:
+                this.corners[piece].ModelTransform.rotate(
+                    -orientation*360/3, -1,1,-1
+                );
+            }
         }
 
         // Edges: 
         for (let i of pieces_involved[1])   {
             // Divide index by 2 to conver from 'stiker index' to 'piece index'
-            const piece = Math.floor(i/2);
+            const pos = Math.floor(i/2);
+            const piece = Math.floor(this.state.edges[pos]/2);
+
             this.edges[piece].ModelTransform = new J3DIMatrix4();
             this.edges[piece].ModelTransform.rotate(angle, x, y, z);
-            this.edges[piece].ModelTransform.multiply(edge_TG[piece]);
+            this.edges[piece].ModelTransform.multiply(edge_TG[pos]);
+
+            const orientation = this.state.edges[pos]%2;
+            if (orientation != 0) {
+                // Orient the corner:
+                this.edges[piece].ModelTransform.rotate(
+                    -orientation*180, -1,1,-1
+                );
+            }
         }
 
         // Centers: 
@@ -104,6 +141,9 @@ export class RubiksCube {
     }
 
     update_state(corners: number[], edges: number[]) {
+        this.state.corners = corners;
+        this.state.edges = edges;
+
         console.log("Updating state: ", corners, edges);
         for (let i = 0; i < corners.length; ++i) {
             const piece = Math.floor(corners[i]/3);
@@ -127,8 +167,12 @@ export class RubiksCube {
             const orientation = edges[i]%2;
             if (orientation != 0) {
                 // Orient the edge:
-                this.corners[piece].ModelTransform.rotate(180, 0,1,-1);
+                this.edges[piece].ModelTransform.rotate(180, 0,1,-1);
             }
+        }
+
+        for (let i in this.centers) {
+            this.centers[i].ModelTransform = center_TG[i]
         }
     }
 }
