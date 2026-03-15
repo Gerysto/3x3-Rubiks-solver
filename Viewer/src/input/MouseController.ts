c8fbe7291a5877a9b912dd18b4e8ee2e44830a5c
Gerard Félez Guerrero <gerard.felez@estudiantat.upc.edu>
2026-03-15 22:20:55 +0100

    Modifies MouseController to enable rotating the cube with on a phone.

--- Viewer/src/input/MouseController.ts
+++ Viewer/src/input/MouseController.ts
@@ -14,10 +14,10 @@ export class MouseController {
 
         this.mouse_pressed = false;
 
-        this.canvas.addEventListener("mousedown", () => this.mouse_pressed = true);
-        this.canvas.addEventListener("mouseup"  , () => this.mouse_pressed = false);
+        this.canvas.addEventListener("pointerdown", () => this.mouse_pressed = true);
+        this.canvas.addEventListener("pointerup"  , () => this.mouse_pressed = false);
 
-        this.canvas.addEventListener("mousemove", (e) => {
+        this.canvas.addEventListener("pointermove", (e) => {
             const new_X = e.clientX - this.canvas.offsetLeft;
             const new_Y = e.clientY - this.canvas.offsetTop;
 
@@ -31,11 +31,12 @@ export class MouseController {
         });
 
         this.canvas.addEventListener("wheel", (e) => {
-            if (e.deltaY > 0 && this.camera.fov > 20) {
+            e.preventDefault();
+            if (e.deltaY < 0 && this.camera.fov > 20) {
                 //console.log("Zoom in: ", e.deltaY);
                 this.camera.fov /= 1.2;
             }
-            else if (e.deltaY < 0 && this.camera.fov < 130){
+            else if (e.deltaY > 0 && this.camera.fov < 130){
                 //console.log("Zoom out!", e.deltaY);
                 this.camera.fov *= 1.2;
             }
