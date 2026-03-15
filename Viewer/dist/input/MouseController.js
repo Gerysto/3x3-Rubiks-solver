7721d843b42a4c6f44176be536fec2146f78786e
Gerard Félez Guerrero <gerard.felez@estudiantat.upc.edu>
2026-03-15 22:21:31 +0100

    Slighly changes the html structure

--- Viewer/dist/input/MouseController.js
+++ Viewer/dist/input/MouseController.js
@@ -5,9 +5,9 @@ export class MouseController {
         this.camera = camera;
         this.canvas = canvas;
         this.mouse_pressed = false;
-        this.canvas.addEventListener("mousedown", () => this.mouse_pressed = true);
-        this.canvas.addEventListener("mouseup", () => this.mouse_pressed = false);
-        this.canvas.addEventListener("mousemove", (e) => {
+        this.canvas.addEventListener("pointerdown", () => this.mouse_pressed = true);
+        this.canvas.addEventListener("pointerup", () => this.mouse_pressed = false);
+        this.canvas.addEventListener("pointermove", (e) => {
             const new_X = e.clientX - this.canvas.offsetLeft;
             const new_Y = e.clientY - this.canvas.offsetTop;
             if (this.mouse_pressed) {
@@ -18,11 +18,12 @@ export class MouseController {
             this.mouseY = new_Y;
         });
         this.canvas.addEventListener("wheel", (e) => {
-            if (e.deltaY > 0 && this.camera.fov > 20) {
+            e.preventDefault();
+            if (e.deltaY < 0 && this.camera.fov > 20) {
                 //console.log("Zoom in: ", e.deltaY);
                 this.camera.fov /= 1.2;
             }
-            else if (e.deltaY < 0 && this.camera.fov < 130) {
+            else if (e.deltaY > 0 && this.camera.fov < 130) {
                 //console.log("Zoom out!", e.deltaY);
                 this.camera.fov *= 1.2;
             }
